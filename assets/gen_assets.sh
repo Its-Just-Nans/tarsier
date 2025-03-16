4d3e4a97c327f293301289f3062dab09639be83f
n4n5 <its.just.n4n5@gmail.com>
2025-03-16 21:05:36 +0100

    update assets

---
+++ assets/gen_assets.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+convert -background none tarsier.svg -resize 192x192 icon_ios_touch_192.png
+convert -background none tarsier.svg -resize 256x256 icon-256.png
+convert -background none tarsier.svg -resize 1024x1024 icon-1024.png
+convert -background none tarsier.svg -resize 512x512 maskable_icon_x512.png
+
+# https://golb.n4n5.dev/utils-linux.html#one-liner-faviconico-generator
+
+TO_ICONIFY=tarsier.svg
+for i in 48 96 144 192; do convert -background none $TO_ICONIFY -resize ${i}x${i} favicon-${i}x${i}.png; done
+convert -background none favicon-* favicon.ico
+rm favicon-*
